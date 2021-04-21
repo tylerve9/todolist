05709965f0bf37b1769079c63f061f90b071612e
Tylerve9 <tyler.ve@gmail.com>
2021-04-20 20:46:09 -0400

    Nuevo diseño iniciando la segunda parte

--- src/js/component/todolist.js
+++ src/js/component/todolist.js
@@ -5,6 +5,121 @@ function TodoList() {
 	const [task, setTask] = useState("");
 	const [error, setError] = useState(false);
 
+	const [userName, setUserName] = useState("")
+	const [isLogin, setIsLogin] = useState(false);
+
+	const baseUrl = 'https://assets.breatheco.de/apis/fake/todos/';
+
+
+
+	function createUser() {
+
+		if (!isLogin) {
+			setError("Error No Login");
+		} else {
+
+			fetch(baseUrl + 'user/' + $userName, {
+				method: "POST",
+				body: JSON.stringify([]),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(resp => {
+					console.log(resp.ok); // will be true if the response is successfull
+					console.log(resp.status); // the status code = 200 or code = 400 etc.
+					console.log(resp.text()); // will try return the exact result as string
+					return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+				})
+				.then(data => {
+					//here is were your code should start after the fetch finishes
+					console.log(data); //this will print on the console the exact object received from the server
+				})
+				.catch(error => {
+					//error handling
+					console.log(error);
+				});
+
+		}
+	}
+
+	function deleteUser(){
+
+
+		fetch(baseUrl + 'todos/' + $userName, {
+			method: "DELETE",
+			body: JSON.stringify([]),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+		//
+		setUserName("");
+	}
+
+	function getInitialTodoList() {
+
+		fetch(baseUrl + userName, {
+			method: "GET",
+			body: JSON.stringify(todos),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+	}
+
+	function setTodoList() {
+		fetch(baseUrl + '', {
+			method: "PUT",
+			body: JSON.stringify(todos),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+	}
+
 	function addTask(params) {
 		if (task == "") {
 			setError(true);
@@ -29,6 +144,33 @@ function TodoList() {
 	return (
 		<div className="container">
 			<div className="row justify-content-center fondo-lista rounded shadow mw-50 mt-5">
+				<div className="col-xs-12 col-md-6  py-3 my-auto perspectiva">
+					<div className="">
+						{
+							(!isLogin) ? (
+								<form action="" method="" onSumbit="">
+									<input className="input" type="text"
+									onChange={ event => setUserName(event.target.value) }  value={ userName } />
+
+									<button className="btn btn-primary" type="submit" >Set User</button>
+
+
+								</form>
+							) : (
+								<div>
+									<h3 className="display-2" >{ userName }</h3>
+								
+								<div>
+									<button className="btn btn-danger" onClick={ deleteUser } type="button" >Delete User</button>
+								</div>
+								</div>
+							)
+						}
+					
+				</div>
+
+				</div>
+
 				<div className="col-xs-12 col-md-6  py-3 my-auto perspectiva">
 					{error && (
 						<div
